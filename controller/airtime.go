a991ed35b91fdaf7542d7466860078b3a2c00f2a
vokes <joshuaomonemu@gmail.com>
2024-07-13 12:07:26 -0700

    my life

--- controller/airtime.go
+++ controller/airtime.go
@@ -12,7 +12,7 @@ func Airtime(w http.ResponseWriter, r *http.Request) {
 	amount := r.Header.Get("Amount")
 	resp, err := models.Airtime(number, amount)
 	if err != nil {
-		io.WriteString(w, "error")
+		io.WriteString(w, err.Error())
 	}
 
 	io.WriteString(w, resp)
