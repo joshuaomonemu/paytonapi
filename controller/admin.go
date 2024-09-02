223b77cb622cf280f676726390ab2039eb6fb042
vokes <joshuaomonemu@gmail.com>
2024-09-02 19:17:40 +0100

    mails

--- controller/admin.go
+++ controller/admin.go
@@ -7,6 +7,9 @@ import (
 )
 
 func Balance(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 	resp, err := models.Balance()
 	if err != nil {
 		io.WriteString(w, err.Error())
